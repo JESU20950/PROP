1bd2eff7fa40365d752be876db4d5b6ce01c2ce1
JESUS <jmr_sultan@hotmail.com>
2019-04-07 20:13:40 +0200

    new repository

--- src/Data/Problem.java
+++ src/Data/Problem.java
@@ -1,5 +1,91 @@
 package Data;
 
+import java.util.Arrays;
+
 public class Problem {
+
+
+        static public boolean iscorrectFen(String FEN){
+            int n_K = 0;
+            int n_k = 0;
+            int n_Q = 0;
+            int n_q = 0;
+            int n_R = 0;
+            int n_r = 0;
+            int n_B = 0;
+            int n_b = 0;
+            int n_P = 0;
+            int n_p = 0;
+            int n_N = 0;
+            int n_n = 0;
+            int w = 0;
+            while (FEN.charAt(w) == ' ') ++w;
+            while (w < FEN.length()) {
+                int eight_elements = 0;
+                while (w < FEN.length() && FEN.charAt(w) != '/') {
+                    switch (FEN.charAt(w)) {
+                        case 'k':
+                            ++n_k;
+                            ++eight_elements;
+                            break;
+                        case 'q':
+                            ++n_q;
+                            ++eight_elements;
+                            break;
+                        case 'r':
+                            ++n_r;
+                            ++eight_elements;
+                            break;
+                        case 'b':
+                            ++n_b;
+                            ++eight_elements;
+                            break;
+                        case 'n':
+                            ++n_n;
+                            ++eight_elements;
+                            break;
+                        case 'p':
+                            ++n_p;
+                            ++eight_elements;
+                            break;
+                        case 'K':
+                            ++n_K;
+                            ++eight_elements;
+                            break;
+                        case 'Q':
+                            ++n_Q;
+                            ++eight_elements;
+                            break;
+                        case 'R':
+                            ++n_R;
+                            ++eight_elements;
+                            break;
+                        case 'B':
+                            ++n_B;
+                            ++eight_elements;
+                            break;
+                        case 'N':
+                            ++n_N;
+                            ++eight_elements;
+                            break;
+                        case 'P':
+                            ++n_P;
+                            ++eight_elements;
+                            break;
+                        default:
+                            int space = Character.getNumericValue(FEN.charAt(w));
+                            if (space >= 0 && space <= 9) eight_elements = eight_elements + space;
+                            else return false;
+                    }
+                    ++w;
+                }
+                if (n_K > 2 || n_k > 2 || n_Q > 1 || n_q > 1 || n_R > 2 || n_r  > 2 || n_b > 2 || n_B > 2 || n_P > 8 || n_p > 8 || n_N > 2 || n_n > 2) return false;
+                if (eight_elements != 8) return false;
+                ++w;
+            }
+            return !(n_K > 2 || n_k > 2 || n_Q > 1 || n_q > 1 || n_R > 2 || n_r  > 2 || n_b > 2 || n_B > 2 || n_P > 8 || n_p > 8 || n_N > 2 || n_n > 2);
+        }
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,17 @@
-import Data.Cell;
-import Data.Table;
-
 import java.util.Scanner;
+
+import static Data.Problem.iscorrectFen;
 
 
 public class Main {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        String s = "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B";
-        Table t = testConstructor(s);
-        Cell[][] t2 = testgettable(t);
-        print_table(t2);
-        System.out.println(" ");
-        boolean b = t.MovePiece(6,5,3,2);
+        String s = input.nextLine();
+        System.out.println(s);
+        boolean b = iscorrectFen(s);
         if (b) System.out.println("bieeenn");
         else System.out.println("ostia neeng");
-        t2 = testgettable(t);
-        print_table(t2);
-
     }
 
-    private static Table testConstructor(String s){
-        return new Table(s);
-    }
-    private static Cell[][] testgettable(Table t){
-        return t.gettable();
-    }
-
-    private static void print_table(Cell[][] table){
-        for (int i = 0; i<8; ++i){
-            for  (int j = 0; j<8; ++j){
-                if (table[i][j].getPiece() == null)System.out.printf("X");
-                else System.out.printf(table[i][j].getPiece().getName());
-
-            }
-            System.out.println(" ");
-        }
-    }
 }
+

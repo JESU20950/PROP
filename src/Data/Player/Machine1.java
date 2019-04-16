8067fe1995ba2c24a926d1390f3ec76a29f93c76
Joan <gomezjoan6@gmail.com>
2019-04-16 17:03:48 +0200

    Humano

--- src/Data/Player/Machine1.java
+++ src/Data/Player/Machine1.java
@@ -1,6 +1,8 @@
 package Data.Player;
 
 public class Machine1 extends Player{
-
+    public Machine1(){
+        super.name = "Machine1";
+    }
 
 }
--- src/Data/Player/Machine2.java
+++ src/Data/Player/Machine2.java
@@ -1,6 +1,9 @@
 package Data.Player;
 
 public class Machine2 extends Player{
+    public Machine2(){
+        super.name = "Machine2";
+    }
 
 
 }

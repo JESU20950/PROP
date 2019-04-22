8d78a6e7cc8cfe8accc1c49eb3dd118cdf88fe36
Joan <gomezjoan6@gmail.com>
2019-04-22 18:10:36 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 import java.util.List;
 
 
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -244,6 +245,7 @@ public class Main {
 
     private static void introduceFEN() throws CloneNotSupportedException, IOException {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Now introduce your FEN: ");
         String s = sc.nextLine();
         boolean b = iscorrectFen(s);
         while (!b) {

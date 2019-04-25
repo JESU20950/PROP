07739c7802b567e9f990ce574d572fd3843b5f06
007kof <alex.ji.chen@gmail.com>
2019-04-25 12:17:28 +0200

    ji

--- src/Domain/Table.java
+++ src/Domain/Table.java
@@ -249,7 +249,7 @@ public class Table implements Cloneable {
     }
 
     public boolean check(boolean player) {
-        if (king_position(!player) == null) return false;
+        if (king_position(!player) == null) return true;
         int i_king = king_position(!player).getI();
         int j_king = king_position(!player).getJ();
         List<Piece> pieces = getPieces(player);
